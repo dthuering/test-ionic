3c8e7749befddf9b63404380bfddbec57215f66e
Danny Thuering <danny@webmobix.com>
2023-04-19 16:43:08 +0200

    test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Keyboard } from '@capacitor/keyboard';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,22 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   constructor() {}
+  ngOnInit(): void {
+    Keyboard.addListener('keyboardWillShow', () => {
+      console.log(document.body.getBoundingClientRect());
+    });
+    Keyboard.addListener('keyboardDidShow', () => {
+      console.log(document.body.getBoundingClientRect());
+      if (document && document.activeElement) {
+        console.log('1', document.activeElement);
+        console.log('1', document.activeElement.getBoundingClientRect());
+        setTimeout(() => {
+          console.log('2', document.activeElement);
+          console.log('2', document.activeElement!.getBoundingClientRect());
+        }, 75);
+      }
+    });
+  }
 }
