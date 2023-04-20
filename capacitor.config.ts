a3a38de46ea6eff085d208287f96c2cac83c5523
Danny Thuering <danny@webmobix.com>
2023-04-20 08:00:48 +0200

    add keyboard fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'SampleKeyboardApp',
   webDir: 'www',
   bundledWebRuntime: false,
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      // style: KeyboardStyle.Dark,
+      resizeOnFullScreen: true, // Android
+    },
+  },
 };
 
 export default config;
